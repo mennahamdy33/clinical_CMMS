17ebd27a5bfbe9902e0055be39b8a1037768fda0
mennahamdy33 <menna.h.m.elsayed@gmail.com>
2020-04-23 18:48:14 +0200

    edited

--- cmms/index.js
+++ cmms/index.js
@@ -122,64 +122,6 @@ function listEvents(auth) {
 }
 app.use('/',main);
 app.use('/',users);
-var Calendar = tui.Calendar;
-
-var calendar = new Calendar('#calendar', {
-
-    // options here
-    // 'day', 'week', 'month'
-    defaultView: 'week',
-
-    // shows the milestone and task in weekly, daily view
-    taskView: true,
-
-    // shows the all day and time grid in weekly, daily view
-    scheduleView: true,
-
-    // template options
-    template: {
-        milestone: function(schedule) {
-            return '<span style="color:red;"><i class="fa fa-flag"></i> ' + schedule.title + '</span>';
-        },
-        milestoneTitle: function() {
-            return 'Milestone';
-        },
-        task: function(schedule) {
-            return '&nbsp;&nbsp;#' + schedule.title;
-        },
-        taskTitle: function() {
-            return '<label><input type="checkbox" />Task</label>';
-        },
-        allday: function(schedule) {
-            return schedule.title + ' <i class="fa fa-refresh"></i>';
-        },
-        alldayTitle: function() {
-            return 'All Day';
-        },
-        time: function(schedule) {
-            return schedule.title + ' <i class="fa fa-refresh"></i>' + schedule.start;
-        }
-    },
-    week: {
-        daynames: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-        startDayOfWeek: 0,
-        narrowWeekend: true
-    },
-    month: {
-        daynames: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-        startDayOfWeek: 0,
-        narrowWeekend: true
-    },
-
-    // list of Calendars that can be used to add new schedule
-    calendars: [],
-
-    // whether use default creation popup or not
-    useCreationPopup: false,
-
-    // whether use default detail popup or not
-    useDetailPopup: false
-});
 
 const port = process.env.PORT || 1000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
